98ace2f3183013069872f26dbee0a76bff4b86ec
kohei-irohasu <yktmd059@icloud.com>
2023-10-17 22:32:31 +0900

    commit

--- ITP_9.py
+++ ITP_9.py
@@ -22,3 +22,25 @@ while True:
     num %= len(s)  # 総和を文字列の長さで割る
     shuffled = s[num:] + s[:num]  # シャッフルした結果の文字列を作成
     print(shuffled)
+
+#9_C
+n = int(input())
+t = 0
+h = 0
+
+for _ in range(n):
+    taro, hanako = input().split()
+    
+    if taro == hanako:
+        t += 1
+        h += 1
+    elif taro > hanako:
+        t += 3
+    else:
+        h += 3
+
+print(t, h)
+
+
+#9_D
+
