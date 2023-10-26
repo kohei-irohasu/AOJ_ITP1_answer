fd9077ccc870b6b6369121318e1fee91c39ac4b0
kohei-irohasu <yktmd059@icloud.com>
2023-10-26 16:03:48 +0900

    commit

--- AOJ_ITP1/ITP_6.py
+++ AOJ_ITP1/ITP_6.py
@@ -27,40 +27,22 @@ find_cards(n, data)
 
 #6_C
 def output(H):
-    for i in range(3):
-        for j in range(10):
-            print(f' {H[i][j]}', end='')
-        print()
-        
-def main():
-    H1 = [[0] * 10 for _ in range(3)]
-    H2 = [[0] * 10 for _ in range(3)]
-    H3 = [[0] * 10 for _ in range(3)]
-    H4 = [[0] * 10 for _ in range(3)]
-    
-    n = int(input())
-    for _ in range(n):
-        h_num, f_num, r_num, div_num = map(int, input().split())
-        
-        if h_num == 1:
-            H1[f_num - 1][r_num - 1] += div_num
-        elif h_num == 2:
-            H2[f_num - 1][r_num - 1] += div_num
-        elif h_num == 3:
-            H3[f_num - 1][r_num - 1] += div_num
-        else:
-            H4[f_num - 1][r_num - 1] += div_num
-            
-    output(H1)
-    print('#' * 20)
-    output(H2)
-    print('#' * 20)
-    output(H3)
-    print('#' * 20)
-    output(H4)
-    
+    for row in H:
+        print(' ', end='')
+        print(*row)
 
-main()
+H = [[[0] * 10 for _ in range(3)] for _ in range(4)]
+n = int(input())
+for _ in range(n):
+    h_num, f_num, r_num, div_num = map(int, input().split())
+    H[h_num - 1][f_num - 1][r_num - 1] += div_num
+
+for i in range(4):
+    output(H[i])
+    if i < 3:
+        print("#" * 20)
+        
+
 
 #6_D
 n, m = map(int, input().split())
